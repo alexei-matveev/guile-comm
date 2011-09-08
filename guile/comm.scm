8d73346baf7c187ba810930a87fc0fcd8ac74187
matveev <matveev@theochem.tu-muenchen.de>
2011-09-08 12:21:46 +0000

    SCM: add cyclic shift based on comm-send-recv

--- guile/comm.scm
+++ guile/comm.scm
@@ -9,34 +9,48 @@
 (define size (comm-size world))
 (define rank (comm-rank world))
 
-;; compute PI in parallel:
+;;
+;; Cyclic shift, send to the right, receive from the left:
+;;
+(let
+  ((right-peer (modulo (+ rank 1) size))
+  (left-peer (modulo (- rank 1) size))
+  (token (+ rank 1000))  ; token to pass, rank specific
+  (message-tag 999)) ; arbitrary number, same for every worker
+    (begin
+      (let loop ((i 0) (token token))
+        (if (< i size)
+          (let
+            ((token (comm-send-recv world right-peer left-peer message-tag token)))
+              (begin
+                (display (list rank token))(newline)
+                (loop (+ i 1) token)))))))
+
+;;
+;; Compute PI in parallel:
+;;
 (define pi
   (comm-pi world 10000000))
 
-(define (display-my-id world)
-  (display "comm = ")(display world)
-  (display " rank = ")(display (comm-rank world))
-  (display " of ")(display (comm-size world)))
-
+;;
 ;; Loop over ranks with communication barriers inbetween
 ;; for proper output formatting (does not always work):
+;;
 (define (for-each-rank world proc)
   (let loop ((p 0) (size (comm-size world)))
     (if (< p size)
       (begin
         (if (= p (comm-rank world)) ; then it is my turn to act ...
           (begin
-            ;;(display "comm = ")(display world)
-            ;;(display " rank = ")(display p)
-            ;;(display " of ")(display size)
-            (display-my-id world)
-            (display " ")
-            (proc) ; lambda without args
+            (let ((result (proc)))
+              (display (list world (comm-rank world) (comm-size world) result)))
             (newline)))
         (comm-barrier world) ; others wait here until I finish ...
         (loop (+ p 1) size)))))
 
-(for-each-rank world (lambda () (display pi)))
+(for-each-rank world (lambda () pi))
+
+; (exit 0)
 
 ;; each worker is either even or odd:
 (define color (modulo rank 2))
@@ -51,34 +65,37 @@
 
 ;; even output first, odd second:
 (comm-barrier world)
-(if (= color 0) (for-each-rank country (lambda () (display pi-2))))
+(if (= color 0) (for-each-rank country (lambda () pi-2)))
 (comm-barrier world)
-(if (= color 1) (for-each-rank country (lambda () (display pi-2))))
+(if (= color 1) (for-each-rank country (lambda () pi-2)))
 
 ;; release communicators:
 (comm-free country)
 
-(comm-barrier world)
+; (comm-barrier world)
 
+;;
+;; FIXME: ugly abstraction:
+;;
 ;; ping-pong, FIXME: this will dead-lock with odd number of workers:
-(let
-  ((right (modulo (+ rank 1) size)) ; right peer
-  (left (modulo (- rank 1) size)) ; left peer
-  (token (+ rank 1000))
-  (tag 999))
-    (begin
-      (if (= color 0) ; even send, odd receive ...
-        (comm-send world right tag token)        ; on even
-        (let ((ping (comm-recv world left tag))) ; on odd
-          (display "rank = ")(display rank)
-          (display " received ping ")(display ping)
-          (newline)))
-      (if (= color 1) ; odd send, even receive ...
-        (comm-send world left tag token)          ; on odd
-        (let ((pong (comm-recv world right tag))) ; on even
-          (display "rank = ")(display rank)
-          (display " received pong ")(display pong)
-          (newline)))))
+; (let
+;   ((right (modulo (+ rank 1) size)) ; right peer
+;   (left (modulo (- rank 1) size)) ; left peer
+;   (token (+ rank 1000))
+;   (tag 999))
+;     (begin
+;       (if (= color 0) ; even send, odd receive ...
+;         (comm-send world right tag token)        ; on even
+;         (let ((ping (comm-recv world left tag))) ; on odd
+;           (display "rank = ")(display rank)
+;           (display " received ping ")(display ping)
+;           (newline)))
+;       (if (= color 1) ; odd send, even receive ...
+;         (comm-send world left tag token)          ; on odd
+;         (let ((pong (comm-recv world right tag))) ; on even
+;           (display "rank = ")(display rank)
+;           (display " received pong ")(display pong)
+;           (newline)))))
 
 ;; required by MPI:
 (comm-finalize)
