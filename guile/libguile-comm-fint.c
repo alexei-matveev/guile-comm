39c93aa7a7ebe21e6d92531b51fd1d93e0801053
matveev <matveev@theochem.tu-muenchen.de>
2011-10-09 20:38:09 +0000

    SCM: libguile-comm-fint.{c,h} represent MPI_Comm as SCM int

---
+++ guile/libguile-comm-fint.c
@@ -0,0 +1,30 @@
+#include <libguile.h>
+#include <mpi.h>
+
+#include "libguile-comm-fint.h"
+
+//
+// Here we use the one-to-one relation between MPI_Comm instances
+// and their integer Fortran representations of type MPI_Fint
+//
+
+//
+// This converts an MPI_Comm to a SCM int:
+//
+SCM scm_from_comm (const MPI_Comm comm) // comm-init wants to return MPI_COMM_WORLD
+{
+    return scm_from_int (MPI_Comm_c2f (comm));
+}
+
+//
+// This converts a SCM int to an MPI_Comm:
+//
+MPI_Comm scm_to_comm (const SCM comm)
+{
+    return MPI_Comm_f2c (scm_to_int (comm));
+}
+
+void init_guile_comm_smob (void)
+{
+    // nothing
+}
---
+++ guile/libguile-comm-fint.h
@@ -0,0 +1,8 @@
+SCM scm_from_comm (const MPI_Comm comm);
+MPI_Comm scm_to_comm (const SCM comm);
+
+#define SCM_FROM_COMM(comm) (scm_from_comm (comm))
+#define SCM_TO_COMM(smob) (scm_to_comm (smob))
+#define SCM_TO_COMM_UNSAFE(smob) SCM_TO_COMM(smob)
+
+void init_guile_comm_smob (void);
