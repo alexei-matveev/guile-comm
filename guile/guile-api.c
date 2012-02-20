ddeae697455bed2aa18187b3afa4862a38ccf999
matveev <matveev@theochem.tu-muenchen.de>
2012-02-20 12:51:40 +0000

    SCM: a few more macros

--- guile/guile-api.c
+++ guile/guile-api.c
@@ -2,6 +2,9 @@
 
 int guile_macro_scm_to_int (SCM obj);
 int guile_macro_scm_is_true (SCM obj);
+int guile_macro_scm_is_symbol (SCM obj);
+int guile_macro_scm_is_null (SCM obj);
+SCM guile_macro_scm_eol (void);
 
 int guile_macro_scm_to_int (SCM obj)
 {
@@ -11,4 +14,19 @@ int guile_macro_scm_to_int (SCM obj)
 int guile_macro_scm_is_true (SCM obj)
 {
   return scm_is_true (obj);
+}
+
+int guile_macro_scm_is_symbol (SCM obj)
+{
+  return scm_is_symbol (obj);
+}
+
+int guile_macro_scm_is_null (SCM obj)
+{
+  return scm_is_null (obj);
+}
+
+SCM guile_macro_scm_eol ()
+{
+  return SCM_EOL;
 }
