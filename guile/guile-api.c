4fb0cbd5cb3a1504c06ad76baa9f0fe39c4e4b14
matveev <matveev@theochem.tu-muenchen.de>
2012-02-21 14:36:04 +0000

    SCM: scm_from_int() is a macro too

--- guile/guile-api.c
+++ guile/guile-api.c
@@ -1,10 +1,18 @@
 #include <libguile.h>
 
+SCM guile_macro_scm_from_int (int i);
 int guile_macro_scm_to_int (SCM obj);
+
 int guile_macro_scm_is_true (SCM obj);
 int guile_macro_scm_is_symbol (SCM obj);
+
 int guile_macro_scm_is_null (SCM obj);
 SCM guile_macro_scm_eol (void);
+
+SCM guile_macro_scm_from_int (int i)
+{
+  return scm_from_int (i);
+}
 
 int guile_macro_scm_to_int (SCM obj)
 {
