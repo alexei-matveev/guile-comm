18ac29b94d1ffc5bfa1e120e199faa23ef470127
matveev <matveev@theochem.tu-muenchen.de>
2012-04-17 22:33:15 +0000

    SCM: untabify, indent

--- guile/test-guile-comm.scm
+++ guile/test-guile-comm.scm
@@ -13,18 +13,18 @@
 ;; Cyclic shift, send to the right, receive from the left:
 ;;
 (let
-  ((right-peer (modulo (+ rank 1) size))
-  (left-peer (modulo (- rank 1) size))
-  (token (list "token of rank" rank "with payload" (+ rank 0.1))) ; token to pass, rank specific
-  (message-tag 999)) ; arbitrary number, same for every worker
-    (begin
-      (let loop ((i 0) (token token))
-        (if (< i size)
+    ((right-peer (modulo (+ rank 1) size))
+     (left-peer (modulo (- rank 1) size))
+     (token (list "token of rank" rank "with payload" (+ rank 0.1))) ; token to pass, rank specific
+     (message-tag 999))      ; arbitrary number, same for every worker
+  (begin
+    (let loop ((i 0) (token token))
+      (if (< i size)
           (let
-            ((token (comm-send-recv world right-peer left-peer message-tag token)))
-              (begin
-                (display (list rank token))(newline)
-                (loop (+ i 1) token)))))))
+              ((token (comm-send-recv world right-peer left-peer message-tag token)))
+            (begin
+              (display (list rank token))(newline)
+              (loop (+ i 1) token)))))))
 
 ;;
 ;; Compute PI in parallel (by calling native code with our communicator):
@@ -39,14 +39,14 @@
 (define (for-each-rank world proc)
   (let loop ((p 0) (size (comm-size world)))
     (if (< p size)
-      (begin
-        (if (= p (comm-rank world)) ; then it is my turn to act ...
-          (begin
-            (let ((result (proc)))
-              (display (list world (comm-rank world) (comm-size world) result)))
-            (newline)))
-        (comm-barrier world) ; others wait here until I finish ...
-        (loop (+ p 1) size)))))
+        (begin
+          (if (= p (comm-rank world))  ; then it is my turn to act ...
+              (begin
+                (let ((result (proc)))
+                  (display (list world (comm-rank world) (comm-size world) result)))
+                (newline)))
+          (comm-barrier world)   ; others wait here until I finish ...
+          (loop (+ p 1) size)))))
 
 (for-each-rank world (lambda () pi))
 
@@ -79,23 +79,21 @@
 ;;
 ;; ping-pong, FIXME: this will dead-lock with odd number of workers:
 (let
-  ((right (modulo (+ rank 1) size)) ; right peer
-  (left (modulo (- rank 1) size)) ; left peer
-  (token (+ rank 1000))
-  (tag 999))
-    (begin
-      (if (= color 0) ; even send, odd receive ...
-        (comm-send world right tag token)        ; on even
+    ((right (modulo (+ rank 1) size))   ; right peer
+     (left (modulo (- rank 1) size))    ; left peer
+     (token (+ rank 1000))
+     (tag 999))
+  (begin
+    (if (= color 0)                       ; even send, odd receive ...
+        (comm-send world right tag token) ; on even
         (let ((ping (comm-recv world left tag))) ; on odd
           (display (list rank "recv ping" ping))
           (newline)))
-      (if (= color 1) ; odd send, even receive ...
-        (comm-send world left tag token)          ; on odd
+    (if (= color 1)                      ; odd send, even receive ...
+        (comm-send world left tag token) ; on odd
         (let ((pong (comm-recv world right tag))) ; on even
           (display (list rank "recv pong" pong))
           (newline)))))
 
 ;; required by MPI:
 (comm-finalize)
-
-;; options for vim:sw=2:expandtab:smarttab:autoindent:syntax
